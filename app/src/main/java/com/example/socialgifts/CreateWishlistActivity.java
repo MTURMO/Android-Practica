34cccc848c15167d18de6338bbfdcb67f5d4bc2b
Renzo Luna <renzo.luna.liendo@gmail.com>
2023-06-09 20:20:33 +0200

    wishlist activity done

--- app/src/main/java/com/example/socialgifts/CreateWishlistActivity.java
+++ app/src/main/java/com/example/socialgifts/CreateWishlistActivity.java
@@ -3,18 +3,21 @@ package com.example.socialgifts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class CreateWishlistActivity extends AppCompatActivity {
     private EditText newWishlistEditText;
     private Button createWishlistButton;
+    private EditText newWishlistDescriptionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_wishlist);
-        newWishlistEditText = findViewById(R.id.wishlist_new);
+        newWishlistEditText = findViewById(R.id.wishlist_new_name);
+        newWishlistDescriptionEditText = findViewById(R.id.wishlist_new_description);
         createWishlistButton = findViewById(R.id.wishlist_new_create_button);
         createWishlistButton.setOnClickListener(view -> goBack());
     }
---
+++ app/src/main/java/com/example/socialgifts/WishlistActivity.java
@@ -0,0 +1,37 @@
+package com.example.socialgifts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.socialgifts.adapters.FeedAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WishlistActivity extends AppCompatActivity {
+    private TextView wishlistNameTextView;
+    private TextView wishlistUserNameTextView;
+    private TextView wishlistNumGiftsTextView;
+    private TextView wishlistDescriptionTextView;
+    private FeedAdapter feedAdapter;
+    private ArrayList<Product> products;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+        wishlistNameTextView = findViewById(R.id.wishlist_activity_name);
+        wishlistUserNameTextView = findViewById(R.id.wishlist_activity_user_name);
+        wishlistNumGiftsTextView = findViewById(R.id.wishlist_activity_num_gifts);
+        wishlistDescriptionTextView = findViewById(R.id.wishlist_activity_description);
+
+        RecyclerView recyclerView = findViewById(R.id.wishlist_activity_recycle);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        feedAdapter = new FeedAdapter(products, this, "all");
+        recyclerView.setAdapter(feedAdapter);
+    }
+}
