5df48db37580817265686750fc1b2ff4d2c913c5
Renzo Luna <renzo.luna.liendo@gmail.com>
2023-06-09 18:26:17 +0200

    wishlist adapter done

--- app/src/main/java/com/example/socialgifts/adapters/WishListAdapter.java
+++ app/src/main/java/com/example/socialgifts/adapters/WishListAdapter.java
@@ -1,9 +1,12 @@
 package com.example.socialgifts.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.socialgifts.R;
 import com.example.socialgifts.WishList;
 
 import java.util.ArrayList;
@@ -24,12 +27,20 @@ public class WishListAdapter extends RecyclerView.Adapter<WishListAdapter.ViewHo
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.wishlist_card, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        WishList wishList = wishLists.get(position);
+        holder.nameTextView.setText(wishList.getName());
+        holder.numGiftsTextView.setText(wishList.getGifts().length);
+        holder.creationDateTextView.setText(wishList.getCreation_date());
+        holder.itemView.setOnClickListener(view -> {
+            //navigation to WishList Activity
+        });
     }
 
     @Override
@@ -38,9 +49,15 @@ public class WishListAdapter extends RecyclerView.Adapter<WishListAdapter.ViewHo
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView nameTextView;
+        TextView numGiftsTextView;
+        TextView creationDateTextView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            nameTextView = itemView.findViewById(R.id.wishlist_card_name);
+            numGiftsTextView = itemView.findViewById(R.id.wishlist_card_num_gifts);
+            creationDateTextView = itemView.findViewById(R.id.wishlist_card_creation_date);
         }
     }
 }
--- app/src/main/java/com/example/socialgifts/fragments/WishListFragment.java
+++ app/src/main/java/com/example/socialgifts/fragments/WishListFragment.java
@@ -3,14 +3,23 @@ package com.example.socialgifts.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.socialgifts.R;
+import com.example.socialgifts.WishList;
+import com.example.socialgifts.adapters.FriendsAdapter;
+import com.example.socialgifts.adapters.WishListAdapter;
+
+import java.util.ArrayList;
 
 public class WishListFragment extends Fragment {
+
+    private ArrayList<WishList> wishLists;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,10 @@ public class WishListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_wish_list, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.wishlist_recycle_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+        WishListAdapter adapter = new WishListAdapter(wishLists, getContext());
+        recyclerView.setAdapter(adapter);
         return view;
     }
 }
